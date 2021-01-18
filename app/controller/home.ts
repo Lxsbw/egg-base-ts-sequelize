82a08cf59345db114de1140c2e2ed7c4126109a0
zhixiong <lxsbw@outlook.com>
2021-01-18 21:57:26 +0800

    Initial commit

---
+++ app/controller/home.ts
@@ -0,0 +1,13 @@
+import { Controller } from 'egg';
+
+export default class HomeController extends Controller {
+  public async index() {
+    const { ctx } = this;
+    ctx.body = await ctx.service.test.sayHi('egg');
+  }
+
+  public async hello() {
+    const { ctx } = this;
+    ctx.body = { mess: 'hello egg typescript' };
+  }
+}
---
+++ config/config.default.ts
@@ -0,0 +1,31 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1610976957944_2840';
+
+  // add your egg config in here
+  config.middleware = [];
+
+  // add your special config in here
+  const bizConfig = {
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`
+  };
+
+  config.cluster = {
+    listen: {
+      port: 7002,
+      hostname: '127.0.0.1' // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
+      // path: '/var/run/egg.sock'
+    }
+  };
+
+  // the return config will combines to EggAppConfig
+  return {
+    ...config,
+    ...bizConfig
+  };
+};
